28c75c109febac5359a67f42fe2341b6423547dc
dominic <dominicrupert@gmail.com>
2022-09-19 13:44:10 -0600

    guiohuihg

--- domallspicenode/server/services/FavoritesService.js
+++ domallspicenode/server/services/FavoritesService.js
@@ -4,5 +4,8 @@ import {dbContext} from "../db/DbContext";
 class FavoritesService {
 
     async toggleFavorites(body, userId){
+        let favorite = await this.getByValues(body, userId)
+        if(!favorite){
+            
         
     }
